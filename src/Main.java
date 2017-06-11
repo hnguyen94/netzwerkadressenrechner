bc744ae333da8c318acff650775b179f47a93e1b
Tom Hüttmann <tom.huettmann@outlook.de>
2017-06-11 15:59:16 +0200

    Add Welcome Message

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
+import javax.swing.*;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -18,10 +20,15 @@ public class Main {
             NetworkCalculator networkCalculator = new NetworkCalculator("Netzwerkadressenrechner", (JSONArray) jsonObject.get("data"));
 
         } catch (IOException | ParseException e) {
+
+
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("data", new JSONArray());
             NetworkCalculator networkCalculator = new NetworkCalculator("Netzwerkadressenrechner", (JSONArray) jsonObject.get("data"));
-            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, "<html>Schön, dass <b>Sie</b> sich für unseren <b>Netzwerkaddressenrechner</b> entschieden haben!<br>" +
+                            "Falls sie <b>Hilfe</b> benötigen, schauen Sie gerne in das <b>Handbuch</b>.<br><br>" +
+                            "Es wurde soeben die <i>data.json</i> Datei für die automatische <b>Datenspeicherung</b> angelegt.</html>",
+                    "Herzlich Willkommen", JOptionPane.PLAIN_MESSAGE);
         }
     }
 
